999b931d6da768185f11a59b4165ee694e694108
cici <2211072043@qq.com>
2022-08-04 17:46:40 +0800

    Site updated: 2022-08-04 17:46:39

--- js/local-search.js
+++ js/local-search.js
@@ -148,7 +148,7 @@
   var searchSelector = '#local-search-input';
   var resultSelector = '#local-search-result';
   modal.on('show.bs.modal', function() {
-    var path = CONFIG.search_path || "/local-search.xml?v=b0f9caf74a";
+    var path = CONFIG.search_path || "/local-search.xml?v=3c62016541";
     localSearchFunc(path, searchSelector, resultSelector);
   });
   modal.on('shown.bs.modal', function() {
